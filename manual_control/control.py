73b40a71c9bd217299e876683fa5ad5d3e4feaaa
LuisAugusto0205 <gutopprado@gmail.com>
2024-09-20 23:30:42 -0300

    hotfix

--- manual_control/control.py
+++ manual_control/control.py
@@ -3,6 +3,6 @@ import getch
 while True:
     key = getch.getch()
     if key == 'f': break
-    with open('command.txt', 'w') as file:
+    with open('manual_control/command.txt', 'w') as file:
         file.write(key)
 
